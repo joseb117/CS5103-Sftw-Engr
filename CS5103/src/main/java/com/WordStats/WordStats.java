d3206902202f1e686c40abdd1acfc3e1f848ad11
joseb117 <93303139+joseb117@users.noreply.github.com>
2022-03-08 19:01:21 -0600

    Added main class and requirements text file

---
+++ CS5103/src/main/java/com/WordStats/WordStats.java
@@ -0,0 +1,15 @@
+/*
+ * Author: Jose Burnias
+ * CS5103 - Software Engineering
+ * Program to calculate different statistics of unique words within a text document
+ */
+package com.WordStats;
+
+public class WordStats {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
